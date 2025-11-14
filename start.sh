c46c99a647b9291efbab1e71504beccc6f57ffa4
773-773 <na77sevensea@gmail.com>
2025-11-14 23:12:14 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-./pocketbase serve --http=0.0.0.0:8080 --dir=/app/pb_data --publicDir=/app/pb_public --hooksDir=/app/pb_hooks
+./pocketbase serve \
+  --http=0.0.0.0:8080 \
+  --dir=/app/pb_data \
+  --publicDir=/app/pb_public \
+  --hooksDir=/app/pb_hooks/templates/mailer
